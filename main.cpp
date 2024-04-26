37841ffe7ed3da75bf47cbe3aae4e63c405a2cf0
Chris Antos <sparrowhawk996@gmail.com>
2024-04-25 19:13:39 -0700

    Basic zoom functionality works.

--- main.cpp
+++ main.cpp
@@ -17,6 +17,15 @@ static const WCHAR c_wndclass_name[] = TEXT("ZoominMainWindow");
 
 static HINSTANCE g_hinst = 0;
 
+template <typename T> T clamp(const T value, const T low, const T high)
+{
+    if (value < low)
+        return low;
+    if (value > high)
+        return high;
+    return value;
+}
+
 //------------------------------------------------------------------------------
 // Registry.
 
@@ -61,6 +70,7 @@ class SizeTracker
 public:
     void OnCreate(HWND hwnd);
     void OnSize();
+    void OnDpiChanged(const DpiScaler& dpi);
     void OnDestroy();
 
 private:
@@ -140,6 +150,11 @@ void SizeTracker::OnSize()
     }
 }
 
+void SizeTracker::OnDpiChanged(const DpiScaler& dpi)
+{
+    m_dpi.OnDpiChanged(dpi);
+}
+
 void SizeTracker::OnDestroy()
 {
     const LONG cx = m_dpi.ScaleTo(m_rcRestore.right - m_rcRestore.left, 96);
@@ -161,7 +176,31 @@ public:
     static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 private:
+    // Main functions.
+    void OnCreate(HWND hwnd);
+    void OnPaint();
+    void OnButtonDown(LPARAM lParam);
+    void OnMouseMove(LPARAM lParam);
+    void OnCancelMode();
+    bool OnCommand(WORD id, WORD code, HWND hwndCtrl);
+    void OnSize();
+    void OnDpiChanged(const DpiScaler& dpi);
+
+    // Internal helpers.
+    void Init();
+    void SetZoomPoint(LPARAM lParam);
+    void GetZoomArea(RECT& rc);
+    void InvertReticle();
+    void PaintZoomRect(HDC hdc=NULL);
+
+private:
+    HWND m_hwnd = NULL;
     DpiScaler m_dpi;
+    POINT m_pt;
+    SIZE m_area;
+    UINT m_factor = 4;
+    RECT m_rcMonitor;
+    bool m_captured = false;
     SizeTracker m_sizeTracker;
 };
 
@@ -173,18 +212,19 @@ LRESULT CALLBACK Zoomin::WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPar
     {
     case WM_ERASEBKGND:
         return true;
-
     case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            BeginPaint(hwnd, &ps);
-            SaveDC(ps.hdc);
+        s_zoomin.OnPaint();
+        break;
 
-// TODO: paint zoomed content.
-
-            RestoreDC(ps.hdc, -1);
-            EndPaint(hwnd, &ps);
-        }
+    case WM_LBUTTONDOWN:
+        s_zoomin.OnButtonDown(lParam);
+        break;
+    case WM_MOUSEMOVE:
+        s_zoomin.OnMouseMove(lParam);
+        break;
+    case WM_LBUTTONUP:
+    case WM_CANCELMODE:
+        s_zoomin.OnCancelMode();
         break;
 
 // TODO: mouse ldown/move/lup to position zoom region.
@@ -194,55 +234,21 @@ LRESULT CALLBACK Zoomin::WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPar
             const WORD id = GET_WM_COMMAND_ID(wParam, lParam);
             const HWND hwndCtrl = GET_WM_COMMAND_HWND(wParam, lParam);
             const WORD code = GET_WM_COMMAND_CMD(wParam, lParam);
-            switch (id)
-            {
-            case IDM_EDIT_COPY:
-// TODO: copy zoomed content to clipboard.
-                break;
-            case IDM_EDIT_REFRESH:
-// TODO: refresh zoomed content.
-                break;
-            case IDM_OPTIONS_GRIDLINES:
-// TODO: toggle gridlines.
-                break;
-            case IDM_OPTIONS_OPTIONS:
-// TODO: Options dialog.
-                break;
-            case IDM_HELP_ABOUT:
-// TODO: About dialog with link to repo.
-                break;
-            case IDM_REFRESH_ONOFF:
-// TODO: toggle auto-refresh.
-                break;
-
-            case IDM_MOVE_UP:
-            case IDM_MOVE_DOWN:
-            case IDM_MOVE_LEFT:
-            case IDM_MOVE_RIGHT:
-// TODO: move zoom region.
-                break;
-
-            default:
+            if (!s_zoomin.OnCommand(id, code, hwndCtrl))
                 goto LDefault;
-            }
         }
         break;
 
+    case WM_WINDOWPOSCHANGED:
+        s_zoomin.OnSize();
+        goto LDefault;
     case WM_DPICHANGED:
-        s_zoomin.m_dpi.OnDpiChanged(DpiScaler(wParam));
-        InvalidateRect(hwnd, nullptr, false);
+        s_zoomin.OnDpiChanged(DpiScaler(wParam));
         break;
 
     case WM_CREATE:
-        SendMessage(hwnd, WM_SETICON, true, LPARAM(LoadImage(g_hinst, MAKEINTRESOURCE(IDI_MAIN), IMAGE_ICON, 0, 0, 0)));
-        SendMessage(hwnd, WM_SETICON, false, LPARAM(LoadImage(g_hinst, MAKEINTRESOURCE(IDI_MAIN), IMAGE_ICON, 16, 16, 0)));
-        s_zoomin.m_sizeTracker.OnCreate(hwnd);
+        s_zoomin.OnCreate(hwnd);
         goto LDefault;
-
-    case WM_WINDOWPOSCHANGED:
-        s_zoomin.m_sizeTracker.OnSize();
-        goto LDefault;
-
     case WM_DESTROY:
         s_zoomin.m_sizeTracker.OnDestroy();
         break;
@@ -256,6 +262,198 @@ LDefault:
     }
 
     return 0;
+}
+
+void Zoomin::OnCreate(HWND hwnd)
+{
+    m_hwnd = hwnd;
+    m_dpi = __GetDpiForWindow(m_hwnd);
+    Init();
+    SendMessage(hwnd, WM_SETICON, true, LPARAM(LoadImage(g_hinst, MAKEINTRESOURCE(IDI_MAIN), IMAGE_ICON, 0, 0, 0)));
+    SendMessage(hwnd, WM_SETICON, false, LPARAM(LoadImage(g_hinst, MAKEINTRESOURCE(IDI_MAIN), IMAGE_ICON, 16, 16, 0)));
+    m_sizeTracker.OnCreate(hwnd);
+}
+
+void Zoomin::OnPaint()
+{
+    PAINTSTRUCT ps;
+    BeginPaint(m_hwnd, &ps);
+    SaveDC(ps.hdc);
+
+    PaintZoomRect(ps.hdc);
+
+    RestoreDC(ps.hdc, -1);
+    EndPaint(m_hwnd, &ps);
+}
+
+void Zoomin::OnButtonDown(LPARAM lParam)
+{
+    SetCapture(m_hwnd);
+    m_captured = true;
+
+    SetZoomPoint(lParam);
+    InvertReticle();
+
+    PaintZoomRect();
+}
+
+void Zoomin::OnMouseMove(LPARAM lParam)
+{
+    if (!m_captured)
+        return;
+
+    InvertReticle();
+    SetZoomPoint(lParam);
+    InvertReticle();
+
+    PaintZoomRect();
+}
+
+void Zoomin::OnCancelMode()
+{
+    if (!m_captured)
+        return;
+
+    InvertReticle();
+
+    ReleaseCapture();
+    m_captured = false;
+}
+
+bool Zoomin::OnCommand(WORD id, WORD code, HWND hwndCtrl)
+{
+    switch (id)
+    {
+    case IDM_EDIT_COPY:
+// TODO: copy zoomed content to clipboard.
+        break;
+    case IDM_EDIT_REFRESH:
+// TODO: refresh zoomed content.
+        break;
+    case IDM_OPTIONS_GRIDLINES:
+// TODO: toggle gridlines.
+        break;
+    case IDM_OPTIONS_OPTIONS:
+// TODO: Options dialog.
+        break;
+    case IDM_HELP_ABOUT:
+// TODO: About dialog with link to repo.
+        break;
+    case IDM_REFRESH_ONOFF:
+// TODO: toggle auto-refresh.
+        break;
+
+    case IDM_MOVE_UP:
+    case IDM_MOVE_DOWN:
+    case IDM_MOVE_LEFT:
+    case IDM_MOVE_RIGHT:
+// TODO: move zoom region.
+        break;
+
+    default:
+        return false;
+    }
+
+    return true;
+}
+
+void Zoomin::OnSize()
+{
+    m_sizeTracker.OnSize();
+
+    RECT rc;
+    GetClientRect(m_hwnd, &rc);
+    m_area.cx = ((rc.right - rc.left) + m_factor - 1) / m_factor;
+    m_area.cy = ((rc.bottom - rc.top) + m_factor - 1) / m_factor;
+}
+
+void Zoomin::OnDpiChanged(const DpiScaler& dpi)
+{
+    m_dpi.OnDpiChanged(dpi);
+    m_sizeTracker.OnDpiChanged(dpi);
+    InvalidateRect(m_hwnd, nullptr, false);
+}
+
+void Zoomin::Init()
+{
+    POINT pt;
+    pt.x = ReadRegLong(TEXT("PointX"), MAXINT);
+    pt.y = ReadRegLong(TEXT("PointY"), MAXINT);
+    SetZoomPoint(MAKELPARAM(pt.x, pt.y));
+
+    m_factor = ReadRegLong(TEXT("ZoomFactor"), 4);
+}
+
+void Zoomin::SetZoomPoint(LPARAM lParam)
+{
+    m_pt.x = SHORT(LOWORD(lParam));
+    m_pt.y = SHORT(HIWORD(lParam));
+    ClientToScreen(m_hwnd, &m_pt);
+
+    MONITORINFO mi = { sizeof(mi) };
+    HMONITOR hmon = MonitorFromPoint(m_pt, MONITOR_DEFAULTTONEAREST);
+    if (!GetMonitorInfo(hmon, &mi))
+    {
+        SetRectEmpty(&m_rcMonitor);
+        return;
+    }
+
+    m_rcMonitor = mi.rcMonitor;
+}
+
+void Zoomin::GetZoomArea(RECT& rc)
+{
+    const LONG xx = clamp(m_pt.x, m_rcMonitor.left + m_area.cx / 2, m_rcMonitor.right - (m_area.cx - m_area.cx / 2));
+    const LONG yy = clamp(m_pt.y, m_rcMonitor.top + m_area.cy / 2, m_rcMonitor.bottom - (m_area.cy - m_area.cy / 2));
+
+    rc.left = xx - m_area.cx / 2;
+    rc.top = yy - m_area.cy / 2;
+    rc.right = rc.left + m_area.cx;
+    rc.bottom = rc.top + m_area.cy;
+}
+
+void Zoomin::InvertReticle()
+{
+    if (m_rcMonitor.right <= m_rcMonitor.left || m_rcMonitor.bottom <= m_rcMonitor.top)
+        return;
+
+    RECT rc;
+    GetZoomArea(rc);
+
+    const LONG thick = 1;
+    InflateRect(&rc, thick, thick);
+
+    HDC hdc = GetDC(NULL);
+    SaveDC(hdc);
+
+    PatBlt(hdc, rc.left, rc.top, thick, rc.bottom - rc.top, DSTINVERT);
+    PatBlt(hdc, rc.right - thick, rc.top, thick, rc.bottom - rc.top, DSTINVERT);
+    PatBlt(hdc, rc.left + thick, rc.top, (rc.right - thick) - (rc.left + thick), thick, DSTINVERT);
+    PatBlt(hdc, rc.left + thick, rc.bottom - thick, (rc.right - thick) - (rc.left + thick), thick, DSTINVERT);
+
+    RestoreDC(hdc, -1);
+    ReleaseDC(NULL, hdc);
+}
+
+void Zoomin::PaintZoomRect(HDC hdc)
+{
+    RECT rc;
+    GetZoomArea(rc);
+
+    if (rc.right <= rc.left || rc.bottom <= rc.top)
+        return;
+
+    const HDC hdcTo = hdc ? hdc : GetDC(m_hwnd);
+    const HDC hdcFrom = GetDC(NULL);
+    const int bltmode = SetStretchBltMode(hdcTo, COLORONCOLOR);
+
+    StretchBlt(hdcTo, 0, 0, m_factor * m_area.cx, m_factor * m_area.cy,
+               hdcFrom, rc.left, rc.top, rc.right - rc.left, rc.bottom - rc.top, SRCCOPY);
+
+    SetStretchBltMode(hdcTo, bltmode);
+    ReleaseDC(NULL, hdcFrom);
+    if (!hdc)
+        ReleaseDC(m_hwnd, hdcTo);
 }
 
 static HWND CreateMainWindow()
