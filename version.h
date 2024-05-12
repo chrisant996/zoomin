f8d842906372b841c1ec9000a836a1b5643dc2b9
Chris Antos <sparrowhawk996@gmail.com>
2024-05-12 16:16:00 -0700

    Increment version.

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #define VERSION_MAJOR           4
-#define VERSION_MINOR           0
+#define VERSION_MINOR           1
 
 #define COPYRIGHT_STR           "Copyright (C) 2024 Christopher Antos"
 
